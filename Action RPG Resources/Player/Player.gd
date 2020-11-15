5405c0dead6e9312ca185e41d8b06346597f3d59
Timi <timi.ogunkeye@gmail.com>
2020-11-15 19:50:08 +0000

    calling animations from player script

--- Action RPG Resources/Player/Player.gd
+++ Action RPG Resources/Player/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-const FRICTION = 500
+const FRICTION = 600
 const ACCELERATION = 500
 const MAX_SPEED = 250
 
@@ -10,6 +10,8 @@ var velocity = Vector2.ZERO
 # var a = 2
 # var b = "text"
 
+onready var animation_player = $AnimationPlayer
+
 func player_movement(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -17,9 +19,18 @@ func player_movement(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animation_player.play("RunRight")
+		elif input_vector.x < 0:
+			animation_player.play("RunLeft")
+		elif input_vector.y > 0:
+			animation_player.play("RunDown")
+		elif input_vector.y < 0:
+			animation_player.play("RunUp")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		print("velocity=>",velocity)
 	else:
+		animation_player.play("IdleRight")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)	
 
@@ -29,6 +40,7 @@ func _physics_process(delta):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#animation_player = $AnimationPlayer
 	print("I'm ready for motion")
 	pass # Replace with function body.
 
